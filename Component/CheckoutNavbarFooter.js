1c796335ad6e4e986efa99351d0471e4a0059330
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-03-06 16:46:33 +0530

    Product-Checkout Pages Done

--- Component/CheckoutNavbarFooter.js
+++ Component/CheckoutNavbarFooter.js
@@ -1,43 +1,59 @@
 function navbar() {
-    return `<div id="navbar">
-    <div class="logo">
-        <img src="https://cdn.freelogovectors.net/wp-content/uploads/2021/02/myntra-logo-freelogovectors.net_.png" alt="Myntra" height="64px" width="84px">
-    </div>
-    <nav>
-        <ul>
-            <li>
-                <a href="../HTML/cart.html" id="currentBag"><b>BAG</b></a></li>
-            <li>----------</li>
-            <li><a href="../HTML/address.html" id="curentAdress">ADDRESS</a></li>
-            <li>----------</li>
-            <li><a href="../HTML/payment.html" id="curentPayment">PAYMENT</a></li>
-        </ul>
-    </nav>
-    <div class="secure-logo">
-        <img src="https://constant.myntassets.com/checkout/assets/img/sprite-secure.png" alt="sprite-secure"><span>100% SECURE<span></div>
-</div>`
+  return ` <div class="header">
+  <div class="logo">
+    <img
+      src="https://cdn.freelogovectors.net/wp-content/uploads/2021/02/myntra-logo-freelogovectors.net_.png"
+      alt="Myntra"
+      height="32px"
+      width="40px"
+    />
+  </div>
+  <nav>
+    <ul>
+      <li>
+        <a href="../HTML/cart.html" id="current_step-1">BAG</a>
+      </li>
+      <li>----------</li>
+      <li><a href="../HTML/address.html" id="current_step-2">ADDRESS</a></li>
+      <li>----------</li>
+      <li>
+        <a href="../HTML/Payment.html" id="current_step-3">PAYMENT</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="secure-logo">
+    <img
+      src="https://constant.myntassets.com/checkout/assets/img/sprite-secure.png"
+      alt="sprite-secure"
+      height="28px"
+      width="25px"
+    />
+    <p>100% SECURE</p>
+  </div>
+</div>
+<hr class="hr1" />`;
 }
 
 function footer() {
-    return `    <div class="CheckoutFooter">
-    <div class="CheckoutFootercontainer">
-        <div class="CheckoutFooterimages">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ssl.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-visa.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-mc.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ae.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-dc.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-nb.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-cod.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-rupay.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-paypal.png">
-            <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-bhim.png">
-        </div>
-        <div class="Help">
-            <div> Need Help ? Contact Us </div>
-        </div>
-    </div>
-</div>`
+  return `    <div class="CheckoutFooter">
+  <div class="CheckoutFootercontainer">
+      <div class="CheckoutFooterimages">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ssl.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-visa.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-mc.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ae.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-dc.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-nb.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-cod.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-rupay.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-paypal.png">
+          <img src="https://constant.myntassets.com/checkout/assets/img/footer-bank-bhim.png">
+      </div>
+      <div class="Help">
+          <div> Need Help ? Contact Us </div>
+      </div>
+  </div>
+</div>`;
 }
 
-export {navbar,footer}
+export { navbar, footer };
--- JS/address.js
+++ JS/address.js
@@ -1,8 +1,135 @@
+import { navbar, footer } from "../Component/CheckoutNavbarFooter.js";
 
-import {navbar,footer} from '../Component/CheckoutNavbarFooter.js'
+let headerEl = document.getElementById("headerEl");
+headerEl.innerHTML = navbar();
 
-let headerEl = document.getElementById('headerEl');
-headerEl.innerHTML = navbar()
+let footerEl = document.getElementById("footerEl");
+footerEl.innerHTML = footer();
 
-let footerEl = document.getElementById('footerEl');
-footerEl.innerHTML = footer()
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("myBtn2");
+var span2 = document.getElementsByClassName("closeBtn2")[0];
+btn2.onclick = function () {
+  modal2.style.display = "block";
+};
+span2.onclick = function () {
+  modal2.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
+var photo = JSON.parse(localStorage.getItem("cart"));
+let discountPrice = JSON.parse(localStorage.getItem("total"));
+let total = 0;
+var price_details = document.querySelector(".payments_details");
+var estimate_delivery = document.querySelector(".delivery-estimate-info-box");
+console.log(photo);
+
+function deliveryUpdates() {
+  for (var i = 0; i < photo.length; i++) {
+    console.log(photo[i].images.image1);
+    // return photo[i].images.image1;
+
+    estimate_delivery.innerHTML = `
+    <div class="delivery-estimate-info">
+      <img
+        src="${photo[i].images.image1}"
+        alt="product"
+        style="height: 48px; width: 36px; display: inline-block"
+      />
+      <div class="deliery-date">
+        <div>
+          <span style="font-family: Whitney, Helvetica, Arial"
+            >ESTIMATED DELIVERY</span
+          >
+          <span
+            style="
+              font-family: Whitney, Helvetica, Arial;
+              font-weight: 600;
+            "
+            >15 MAR 2022</span
+          >
+        </div>
+      </div>
+    </div>
+  </div>`;
+  }
+}
+deliveryUpdates();
+// price_details.innerHTML = ` <h2 style="color: rgb(61, 61, 61); font-size: 18px; padding: 10px">
+// Price Details
+// </h2>
+// <hr />
+// <div
+// style="
+//   color: rgb(36, 36, 36);
+//   font-size: 13px;
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 10px;
+// "
+// >
+// <div>Total MRP</div>
+// <div>${total()}</div>
+// </div>
+// <div
+// style="
+//   color: rgb(36, 36, 36);
+//   font-size: 13px;
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 10px;
+// "
+// >
+// <div>Discounted on</div>
+// <div>MRP -${discount()}</div>
+// </div>
+// <div
+// style="
+//   color: rgb(36, 36, 36);
+//   font-size: 13px;
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 10px;
+// "
+// >
+// <div>Convenience fee</div>
+// <div><b>Free</b></div>
+// </div>
+// <hr />
+// <div
+// style="
+//   color: rgb(36, 36, 36);
+//   font-size: 20px;
+//   font-weight: 400px;
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 10px;
+// "
+// >
+// <div>Total</div>
+// <div>${discountPrice.total}</div>
+// </div>
+// <hr />
+// <button id="Place_Order">
+// <a href="../HTML/Payment.html" style="color: white"
+//   >Make Payment</a
+// >
+// </button>
+// `;
